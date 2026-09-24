f68a507fc7553257a9f1e6186abd45dabb44ac5f
GregDev <storytimewithgreg1@gmail.com>
2026-09-24 11:14:54 +0000

    feat(models): add course and downloads models with relationships

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     //
+    public function papers(){
+        return $this->hasMany(Paper::class, "uploaded_by");
+    }
+    
 }
--- app/Models/Download.php
+++ app/Models/Download.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Download extends Model
 {
     //
+    public function user(){
+        return $this->belongsTo(User::class, "user_id");
+    }
+
+    public function paper(){
+        return $this->belongsTo(Paper::class, "paper_id");
+    }
 }
